c075d92166ab36dd0f4539e5ecf283a750692bd6
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-05-27 14:45:19 +0300

    Refactor navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,16 @@ import { Link } from 'react-router-dom'
 export default class Header extends Component {
   render() {
     return (
-      <section className="section">
+      <nav className="navbar">
         <div className="container">
-          <h1 className="title">Hello World</h1>
-          <p className="subtitle">My first website with <strong>Bulma</strong>!</p>
-          <nav className="navbar">
-            <div className="navbar-menu">
-              <div className="navbar-start">
-                <Link to="/" className="navbar-item">Home</Link>
-                <Link to="/users/sign_in" className="navbar-item">Sign in</Link>
-              </div>
-            </div>
-          </nav>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item">Главная</Link>
+            <Link to="/users/sign_in" className="navbar-item">Войти</Link>
+          </div>
         </div>
-      </section>
+        </div>
+      </nav>
     )
   }
 }
