18ab8a25877e3f560f77b87c4120437567fc5dd9
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-05-27 14:45:26 +0300

    Add sign in form

--- src/components/Sessions/New.js
+++ src/components/Sessions/New.js
@@ -3,9 +3,34 @@ import React, { Component } from 'react'
 export default class New extends Component {
   render() {
     return (
-      <form>
-        Sign in Page
-      </form>
+      <section className="section">
+        <div className="container">
+          <h1 className="title">Представьтесь</h1>
+          <div class="columns">
+            <div class="column is-half">
+              <form>
+                <div className="field">
+                  <label className="label">Почта</label>
+                  <div className="control">
+                    <input className="input" type="email" />
+                  </div>
+                </div>
+                <div className="field">
+                  <label className="label">Пароль</label>
+                  <div className="control">
+                    <input className="input" type="password" />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="control">
+                    <button className="button is-primary">Sign In</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     )
   }
 }
