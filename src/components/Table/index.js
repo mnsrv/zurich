150bfb71dca0cae1123be7911eedc580ffdd9551
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-06-14 02:12:04 +0300

    Table sizes

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,54 +3,101 @@ import classNames from 'classnames'
 
 export default class GalleonTable extends Component {
   render() {
-    const { columns, data } = this.props
+    const { data } = this.props
 
     return (
       <div className="table is-fullwidth">
         <div className="thead">
-          <div className="tr">
-            {columns.map(column => <div key={column.value} className="th">{column.label}</div>)}
-          </div>
+          {this.renderHeadRow()}
         </div>
         <div className="tbody">
-          {this.renderAdding()}
-          {data.map(row => (
-            <div className="tr" key={row.id}>
-              {columns.map(column => <div key={column.value} className="td">{row[column.value]}</div>)}
-            </div>
-          ))}
+          {this.renderAddingRows()}
+          {data.map(this.renderRow)}
         </div>
       </div>
     )
   }
 
-  renderAdding = () => {
-    const { columns, isAdding } = this.props
+  renderHeadRow = () => {
+    const { columns } = this.props
+
+    return (
+      <div className="tr">
+        {columns.map(column => {
+          const cellClassName = classNames('th', { [`th-${column.value}`]: true })
+
+          return <div key={column.value} className={cellClassName}>{column.label}</div>
+        })}
+      </div>
+    )
+  }
+
+  renderRow = (row) => {
+    const { columns } = this.props
+
+    return (
+      <div className="tr" key={row.id}>
+        {columns.map(column => {
+          const cellClassName = classNames('td', { [`td-${column.value}`]: true })
+
+          return <div key={column.value} className={cellClassName}>{row[column.value]}</div>
+        })}
+      </div>
+    )
+  }
+
+  renderAddRow = () => {
+    const { columns } = this.props
+
+    return (
+      <div className="tr is-selected" key="add">
+        {columns.map(column => (
+          <div key={column.value} className="td is-adding">
+            <input className="input" type="text" ref={node => this[column.value] = node} />
+          </div>
+        ))}
+      </div>
+    )
+  }
+
+  renderAddActionsRow = () => {
+    const { columns } = this.props
+
+    return (
+      <div key="add-actions" className="tr is-selected">
+        {columns.map((column, index) => {
+          const isLast = index === columns.length - 1
+          const className = classNames('td', {
+            'action-buttons-container': isLast
+          })
+
+          return (
+            <div key={column.value} className={className}>
+              {isLast
+                ? (
+                  <div className="action-buttons">
+                    <button className="button" onClick={this.cancel}>Отмена</button>
+                    <button className="button is-primary" onClick={this.save}>Сохранить</button>
+                  </div>
+                )
+                : '   '}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  renderAddingRows = () => {
+    const { isAdding } = this.props
 
     if (!isAdding) {
       return null
     }
 
     return [
-      <div key="add" className="tr is-selected">
-        {columns.map(column => (
-          <div key={column.value} className="td is-adding">
-            <input className="input" type="text" ref={node => this[column.value] = node} />
-          </div>
-        ))}
-      </div>,
-      <div key="add-actions" className="tr is-selected">
-        {columns.map((column, index) => (
-          <div key={column.value} className={classNames('td', { 'action-buttons-container': index === columns.length - 1 })}>
-            {index === columns.length - 1 ? (
-              <div className="buttons action-buttons">
-                <button className="button" onClick={this.cancel}>Отмена</button>
-                <button className="button is-primary" onClick={this.save}>Сохранить</button>
-              </div>
-            ) : '   '}
-          </div>
-        ))}
-      </div>
+      this.renderAddRow(),
+      this.renderAddActionsRow()
     ]
   }
 
