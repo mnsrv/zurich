0a5b92240dc02864be1b5d9f0b6b3b4fe1c01442
Aleksandr Mansurov <sasha.mansurov@gmail.com>
2018-07-09 01:10:54 +0300

    Render all transactions with new Table

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import { extendObservable } from 'mobx'
 import { inject, observer } from 'mobx-react'
 
 import stores from '../../stores'
 
-import Row from './Row'
-import Spinner from '../Spinner'
+import Table from '../Table'
 
 @inject('endpoint')
 @observer
@@ -16,14 +14,27 @@ export default class Transactions extends Component {
     const { endpoint, match } = props
     const { budgetId } = match.params
 
-    extendObservable(this, {
-      transactions: new stores.Transaction(endpoint, `v1/${budgetId}`)
-    })
+    this.transactions = new stores.Transaction(endpoint, `v1/${budgetId}`)
 
-    this.state = {
-      isAdding: false,
-      editedId: ''
-    }
+    // TODO: select accounts
+    // TODO: render account name instead of id
+    this.columns = [{
+      label: 'Дата',
+      value: 'date',
+      type: 'date'
+    }, {
+      label: 'Счёт',
+      value: 'account_id',
+      type: 'text'
+    }, {
+      label: 'Заметка',
+      value: 'memo',
+      type: 'text'
+    }, {
+      label: 'Сумма',
+      value: 'amount',
+      type: 'number'
+    }]
   }
 
   componentDidMount() {
@@ -33,93 +44,30 @@ export default class Transactions extends Component {
   render() {
     return (
       <section className="section">
-        <div className="container">
-          <h1 className="title">Все счета</h1>
-          <div className="field is-grouped">
-            <button className="button" onClick={this.startAdding}>Добавить</button>
-          </div>
-          {this.renderTable()}
-        </div>
+        <h1 className="title">Все счета</h1>
+        {this.renderTable()}
       </section>
     )
   }
 
-  renderAdding = () => {
-    if (!this.state.isAdding) {
-      return null
-    }
-    const newCell = {
-      date: '',
-      memo: '',
-      amount: 0
-    }
-    return (
-      <Row
-        key="new"
-        id="new"
-        {...newCell}
-        isEdited={true}
-        onClick={this.doNothing}
-        cancelEdit={this.cancelAdding}
-      />
-    )
-  }
-
   renderTable = () => {
-    const { isLoading } = this.transactions
-
-    if (isLoading) {
-      return <Spinner />
-    }
+    const { collection } = this.transactions
 
     return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Дата</th>
-            <th>Заметка</th>
-            <th>Сумма</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.renderAdding()}
-          {this.renderRows()}
-        </tbody>
-      </table>
+      <Table
+        data={collection}
+        columns={this.columns}
+        updateTransaction={this.updateTransaction}
+      />
     )
   }
 
-  renderRows = () => {
-    const { collection, create } = this.transactions
-
-    return collection.map(({ id, ...props }) => (
-      <Row
-        key={id.toString()}
-        id={id}
-        {...props}
-        create={create}
-        isEdited={id === this.state.editedId}
-        onClick={(e) => this.editCell(id, e)}
-        cancelEdit={this.cancelEdit}
-      />
-    ))
+  updateTransaction = (transaction, callback) => {
+    this.transactions.update({ accounts: transaction.account_id, id: transaction.id }, { transaction }, {
+      200: (response) => {
+        this.transactions.modifyInCollection(response.data.transaction)
+        callback()
+      }
+    })
   }
-
-  startAdding = () => {
-    this.setState({ isAdding: true })
-  }
-
-  cancelAdding = () => {
-    this.setState({ isAdding: false })
-  }
-
-  editCell = (id) => {
-    this.setState({ editedId: id })
-  }
-
-  cancelEdit = () => {
-    this.setState({ editedId: '' })
-  }
-
-  doNothing = () => { }
 }
